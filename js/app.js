1b6d0f183d739399418d5a7ba4b0f9782482cd47
Crystal R. Williams <cortney0215@gmail.com>
2018-06-20 09:27:36 -0400

    add in logic

--- js/app.js
+++ js/app.js
@@ -1,7 +1,35 @@
+let totalMoves = 0;
+const moves = document.querySelector('.moves');
+moves.textContent = totalMoves;
+// moves.addEventListener('change', function() {
+//     document.querySelector('.moves').textContent = totalMoves;
+// });
+
+
 /*
  * Create a list that holds all of your cards
  */
 
+ const cards = [
+     'anchor',
+     'bolt',
+     'cube',
+     'leaf',
+     'bicycle',
+     'diamond',
+     'bomb',
+     'paper-plane-o',
+     'anchor',
+     'bolt',
+     'cube',
+     'leaf',
+     'bicycle',
+     'diamond',
+     'bomb',
+     'paper-plane-o'
+ ];
+
+ let openedCards = [];
 
 /*
  * Display the cards on the page
@@ -9,6 +37,28 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+const shuffledCards = shuffle(cards);
+console.log('shuffledCards ', shuffledCards);
+
+let cardsFinished = 0;
+const cardLength = cards.length;
+const fragment = document.createDocumentFragment();
+while(cardsFinished < cardLength) {
+    
+    const listElement = document.createElement('li');
+    listElement.className = 'card';
+
+    const icon = document.createElement('i');
+    icon.classList.add('fa', `fa-${cards[cardsFinished]}`);
+    listElement.appendChild(icon);
+
+    fragment.appendChild(listElement);
+    cardsFinished++;
+}
+
+const deck = document.querySelector('.deck');
+deck.appendChild(fragment);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -36,3 +86,44 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function incrementMoves() {
+    totalMoves++;
+    moves.textContent = totalMoves;
+}
+
+function addToOpen(target) {
+
+    openedCards.push(target);
+    const openedCardsLen = openedCards.length;
+
+    if (openedCardsLen > 1) {
+        for(let i=0; i < openedCardsLen-1; i++) {
+            console.log(i);
+            if (target.firstChild === openedCards[i].firstChild) {
+                // LOCK THE CARDS IN THE OPEN POSITION
+                target.classList.add('match');
+                openedCards[i].classList.add('match');
+                console.log('match');
+            } else {
+                openedCards[i].classList.remove('open', 'show');
+                openedCards.slice(i);
+                console.log('openedCards ', openedCards);
+                incrementMoves();
+            }
+        }
+    }
+}
+
+
+
+
+function openCard(e) {
+    const cardClicked = e.target;
+    cardClicked.classList.add('open', 'show');
+    console.log('event ', e);
+    // addToOpen(e.target);
+    setTimeout(addToOpen(e.target), 20000);
+}
+
+deck.addEventListener('click',  openCard);
